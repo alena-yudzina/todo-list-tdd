c0039e5738887974c49c07e8920631e0e074b3ae
Елена Юдина <elena@MacBook-Air-Elena.local>
2022-06-28 16:22:09 +0500

    настроили адресацию и убрали лишние тесты

--- lists/views.py
+++ lists/views.py
@@ -14,3 +14,9 @@ def view_list(request):
     """Представление списка"""
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    """Новый список"""
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/only-list/')
